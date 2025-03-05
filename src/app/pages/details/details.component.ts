2290701484e7e9925067ac162e1f540c17ad2d34
Your Name <you@example.com>
2025-03-05 06:02:52 +0200

    details add to cart

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -4,6 +4,8 @@ import { ProuductsService } from '../../core/services/product/prouducts.service'
 import { IData } from '../../shared/interfaces/iproduct';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../core/services/cart/cart.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
     selector: 'app-details',
     imports: [CarouselModule, CurrencyPipe],
@@ -13,6 +15,8 @@ import { CurrencyPipe } from '@angular/common';
 export class DetailsComponent implements OnInit {
   private readonly activatedRoute =inject(ActivatedRoute);
   private readonly prouductsService =inject(ProuductsService);
+  private readonly cartService = inject(CartService);
+  private readonly toastrService = inject(ToastrService);
   productID:any;
   productDetails: IData = {} as IData;
 
@@ -32,7 +36,7 @@ export class DetailsComponent implements OnInit {
         items: 1,
       },
       1000: {
-        items: 1,  
+        items: 1,
       }
     }
   }
@@ -61,5 +65,21 @@ export class DetailsComponent implements OnInit {
         console.log(err)
       }
     })
+  }
+
+
+  addtoCarts(id:string):void {
+    this.cartService.addProductToCart(id).subscribe({
+      next: (res) => {
+        console.log(res);
+        if (res.status == "success") {
+          this.toastrService.success('Product Added Successfully', 'Success');
+          this.cartService.cartnumber.set(res.numOfCartItems)
+        }
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
 }
